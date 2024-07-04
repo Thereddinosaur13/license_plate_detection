9b65b61545e43ba2039089982e1e300b99b298ac
Thereddinosaur13 <152376825+Thereddinosaur13@users.noreply.github.com>
2024-07-04 11:14:03 +0700

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="licensePlateDetection",
+    version="0.0.1",
+    author="Thereddinosaur",
+    author_email="nhiyt13@gmail.com",
+    packages=find_packages(),
+    requires = []   
+)
